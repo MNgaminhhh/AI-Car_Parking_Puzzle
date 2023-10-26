3396fc6ed4042d82a20767a590ff2d5c142a897a
MN <57828955+MNgaminhhh@users.noreply.github.com>
2023-10-26 12:03:50 +0700

    update display bg and button to image

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ class MyGame:
     def create_car(self):
         for car in self.problem_text:
             new_car = Car(self, car[0], car[3], int(car[1]), int(car[2]))
+            
             self.cars.add(new_car)
         
     def new_game(self):
@@ -40,12 +41,15 @@ class MyGame:
 
     def btn_init(self):
         self.btn_list = []
-        list_btn = ['New Game', 'Create', 'Reset']
+        list_btn = ['assets/buttonNewGame.png', 'assets/buttonCreate.png', 'assets/buttonReset.png']
         tab_x = self.settings.tab_x_btn
         tab_y = self.settings.tab_y_btn
         height = self.settings.btn_height
-        for i, text in enumerate(list_btn):
-            self.btn_list.append(Button(self, tab_x, (tab_y + height)*i + self.playing_area.rect.y, text))
+        button_spacing = 100
+        for i, image_path in enumerate(list_btn):
+            y_position = (tab_y + height + button_spacing) * i + self.playing_area.rect.y
+            self.btn_list.append(Button(self, tab_x, y_position, image_path))
+
 
     def check_event(self):
         for event in pygame.event.get() :
