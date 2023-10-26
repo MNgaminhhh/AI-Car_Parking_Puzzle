635f0195a5a1f6e7bf1a0303faba4d0d76765425
MN <57828955+MNgaminhhh@users.noreply.github.com>
2023-10-26 08:48:23 +0700

    create src directory file

---
+++ src/settings.py
@@ -0,0 +1,18 @@
+class Settings:
+    def __init__(self):
+        #screen settings:
+        self.screen_height = 800
+        self.screen_width = 1200
+        self.fps = 60
+        self.bg_color = (82, 114, 242)
+        #game settings:
+        self.map_height = 6
+        self.map_width = 8
+        self.tile_size =64
+        #button settings:
+        self.btn_height = 40
+        self.btn_width = 100
+        self.tab_x_btn = 50
+        self.tab_y_btn = 10
+        self.btn_color = (251, 236, 178)
+        self.font_color = (7, 37, 65)
