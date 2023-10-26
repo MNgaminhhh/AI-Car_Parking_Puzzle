d67144c0a1c46b2dd3f330da8642da153b8c74ce
MtLuth <111564276+MtLuth@users.noreply.github.com>
2023-10-26 13:14:56 +0700

    Khong chinh j het

--- src/settings.py
+++ src/settings.py
@@ -1,7 +1,6 @@
 class Settings:
     def __init__(self):
         #screen settings:
-        #screen settings:
         self.screen_height = 800
         self.screen_width = 1200
         self.fps = 60
